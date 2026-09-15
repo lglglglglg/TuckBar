2d37477f7a40b9da088de69996d228dedd61fc69
LG <liganglig@outlook.com>
2026-09-15 22:53:15 +0800

    feat: simplify TuckBar mark

--- Sources/Brand.swift
+++ Sources/Brand.swift
@@ -8,25 +8,25 @@ enum Brand {
         let image = NSImage(size: NSSize(width: size, height: size), flipped: false) { rect in
             if colored {
                 let background = NSBezierPath(roundedRect: rect.insetBy(dx: size * 0.04, dy: size * 0.04), xRadius: size * 0.22, yRadius: size * 0.22)
-                NSGradient(starting: NSColor(red: 0.32, green: 0.29, blue: 0.88, alpha: 1), ending: NSColor(red: 0.10, green: 0.68, blue: 0.70, alpha: 1))!.draw(in: background, angle: -45)
+                NSColor(red: 0.11, green: 0.13, blue: 0.18, alpha: 1).setFill()
+                background.fill()
             }
             (colored ? NSColor.white : NSColor.black).setStroke()
             let path = NSBezierPath()
             path.lineWidth = size * (colored ? 0.055 : 0.08)
             path.lineCapStyle = .round
             path.lineJoinStyle = .round
-            let inset: CGFloat = colored ? 0.25 : 0.13
+            let inset: CGFloat = colored ? 0.22 : 0.08
             let left = size * inset
             let right = size * (1 - inset)
-            path.move(to: NSPoint(x: left, y: size * 0.66))
-            path.line(to: NSPoint(x: right, y: size * 0.66))
-            path.move(to: NSPoint(x: left, y: size * 0.61))
-            path.line(to: NSPoint(x: left, y: size * 0.31))
-            path.line(to: NSPoint(x: right, y: size * 0.31))
-            path.line(to: NSPoint(x: right, y: size * 0.61))
-            path.move(to: NSPoint(x: size * 0.38, y: size * 0.50))
-            path.line(to: NSPoint(x: size * 0.50, y: size * 0.39))
-            path.line(to: NSPoint(x: size * 0.62, y: size * 0.50))
+            for (y, width) in [(0.68, 0.72), (0.50, 0.56), (0.32, 0.40)] {
+                let rowLeft = size * (0.5 - width / 2)
+                let rowRight = size * (0.5 + width / 2)
+                path.move(to: NSPoint(x: rowLeft, y: size * y))
+                path.line(to: NSPoint(x: rowRight, y: size * y))
+            }
+            path.move(to: NSPoint(x: size * 0.76, y: size * 0.32))
+            path.line(to: NSPoint(x: size * 0.82, y: size * 0.32))
             path.stroke()
             return true
         }
